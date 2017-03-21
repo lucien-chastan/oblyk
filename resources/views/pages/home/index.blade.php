033f3d86bca6b2b06016a683e7fc94b651363136
lucien-chastan <chastanlucien@gmail.com>
2017-03-21 20:55:38 +0100

    avancement dans la page d'accueil

--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -2,13 +2,19 @@
 
 @section('css')
     {{--css particulier à la page--}}
-    {{--<link href="{{url('/')}}/css/home.css" rel="stylesheet">--}}
+    <link href="/css/home.css" rel="stylesheet">
 @endsection
 
 @section('content')
 
     <div class="parallax-container">
-        <div class="parallax"><img src="img/oblyk-home-baume-rousse.jpg"></div>
+        <div class="parallax">
+            <img class="img-parallax-home" src="/img/oblyk-home-chapeau-napoleon.jpg">
+            <div class="div-entete-oblyk">
+                <h1>Oblyk</h1>
+                <p>Site communautaire dédié à l'escalade</p>
+            </div>
+        </div>
     </div>
 
 @endsection
