87bd17a1a16a212ed1b61ca59e409237861caa54
lucien-chastan <chastanlucien@gmail.com>
2017-04-10 23:19:17 +0200

    avancement dans la popup de la grande carte

--- resources/views/pages/map/map.blade.php
+++ resources/views/pages/map/map.blade.php
@@ -23,7 +23,13 @@
 
         //boucle sur les falaises pour ajouter les marqueurs sur la carte
         @foreach($crags as $crag)
-            L.marker([{{$crag['lat']}},{{$crag['lng']}}], {icon: marker_{{$crag['type_voie']}}{{$crag['type_grande_voie']}}{{$crag['type_bloc']}}{{$crag['type_deep_water']}}}).bindPopup('<a href="/site-escalade/{{$crag['id']}}/{{str_slug($crag['label'])}}">{{$crag['label']}}</a><br>').addTo(map);
+            L.marker(
+                [{{$crag['lat']}},{{$crag['lng']}}],
+                {icon: marker_{{$crag['type_voie']}}{{$crag['type_grande_voie']}}{{$crag['type_bloc']}}{{$crag['type_deep_water']}}}
+            )
+            .bindPopup(
+                '<img class="photo-couve-site-leaflet" src="/storage/photos/crags/oblyk-home-baume-rousse.jpg" alt="photo de couverture de {{$crag['label']}}"><div class="crag-leaflet-info"><h2 class="loved-king-font titre-crag-leaflet"><a href="/site-escalade/{{$crag['id']}}/{{str_slug($crag['label'])}}">{{$crag['label']}}</a></h2><span>site de voie et grande-voie, de 150 lignes</span></div>'
+            ).addTo(map);
         @endforeach
 
         //passage de la barre de navigation en noir
