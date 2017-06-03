999ee90a961718019ada8b74144745d31bdd1e92
lucien-chastan <chastanlucien@gmail.com>
2017-06-03 17:59:03 +0200

    ajout d'un message 'pas de ligne'

--- resources/views/pages/crag/vues/sector-vues/sectorLinesVue.blade.php
+++ resources/views/pages/crag/vues/sector-vues/sectorLinesVue.blade.php
@@ -1,41 +1,48 @@
 @inject('Helpers','App\Lib\HelpersTemplates')
 
-<table class="striped responsive-table">
-    <tr>
-        <th>Note</th>
-        <th>Cote</th>
-        <th>Nom</th>
-        <th>Type</th>
-        <th>Hauteur</th>
-        <th>Année</th>
-        <th>Ouvreur</th>
-    </tr>
-@foreach($routes as $route)
+@if(count($routes) > 0)
 
-    <tr data-activates="slide-route" class="button-open-route">
-        <td><img {!! $Helpers::tooltip('Évaluation sur ' . $route->nb_note . ' note(s)') !!} src="/img/note_{{$route->note}}.png" alt="" class="tooltipped img-note-route-sector"></td>
-        <td>
-            @if(count($route->routeSections) > 1)
-                {!! count($route->routeSections) !!} L.
-            @else
-                <span class="color-grade-{{$route->routeSections[0]->grade_val}}">{{$route->routeSections[0]->grade}}{{$route->routeSections[0]->sub_grade}}</span>
-            @endif
-        </td>
-        <td>{{$route->label}}</td>
-        <td><img src="/img/climb-{{$route->climb_id}}.png" alt="" class="type-ligne"> {{$route->climb->label}}</td>
-        <td>
-            @if(count($route->routeSections) == 1 && $route->height >= 35)
-                <i {!! $Helpers::tooltip('Attention voie de plus de 35 mètres') !!} class="tooltipped material-icons red-text left">report_problem</i>
-            @endif
-            {{$route->height}} mètres
-        </td>
-        <td>{{$route->open_year}}</td>
-        <td>{{$route->opener}}</td>
-    </tr>
+    <table class="striped responsive-table">
+        <tr>
+            <th>Note</th>
+            <th>Cote</th>
+            <th>Nom</th>
+            <th>Type</th>
+            <th>Hauteur</th>
+            <th>Année</th>
+            <th>Ouvreur</th>
+        </tr>
+    @foreach($routes as $route)
 
-@endforeach
-</table>
+        <tr data-activates="slide-route" class="button-open-route">
+            <td><img {!! $Helpers::tooltip('Évaluation sur ' . $route->nb_note . ' note(s)') !!} src="/img/note_{{$route->note}}.png" alt="" class="tooltipped img-note-route-sector"></td>
+            <td>
+                @if(count($route->routeSections) > 1)
+                    {!! count($route->routeSections) !!} L.
+                @else
+                    <span class="color-grade-{{$route->routeSections[0]->grade_val}}">{{$route->routeSections[0]->grade}}{{$route->routeSections[0]->sub_grade}}</span>
+                @endif
+            </td>
+            <td>{{$route->label}}</td>
+            <td><img src="/img/climb-{{$route->climb_id}}.png" alt="" class="type-ligne"> {{$route->climb->label}}</td>
+            <td>
+                @if(count($route->routeSections) == 1 && $route->height >= 35)
+                    <i {!! $Helpers::tooltip('Attention voie de plus de 35 mètres') !!} class="tooltipped material-icons red-text left">report_problem</i>
+                @endif
+                {{$route->height}} mètres
+            </td>
+            <td>{{$route->open_year}}</td>
+            <td>{{$route->opener}}</td>
+        </tr>
 
+    @endforeach
+    </table>
+
+@else
+
+    <p class="grey-text text-center">Il n'y a pas de ligne référencée sur ce secteur,<br> vous pouvez en ajouter en cliquant sur le bouton +</p>
+
+@endif
 {{--BOUTON POUR AJOUTER UN LIGNE--}}
 @if(Auth::check())
     <div class="row">
