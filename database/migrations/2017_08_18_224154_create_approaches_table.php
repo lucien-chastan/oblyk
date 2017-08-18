4ab2e35397aed6f1508820bc5de68ab47bd70a43
lucien-chastan <chastanlucien@gmail.com>
2017-08-18 23:50:33 +0200

    migration des marches d'approches

---
+++ database/migrations/2017_08_18_224154_create_approaches_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApproachesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('approaches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('crag_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->text('polyline');
+            $table->text('description');
+            $table->integer('length');
+            $table->timestamps();
+
+            //clé étrangère
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('crag_id')->references('id')->on('crags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('approaches');
+    }
+}
