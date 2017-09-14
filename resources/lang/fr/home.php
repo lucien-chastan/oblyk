7860d667a68b40f7b88faefe99375e64eccc5bbf
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 09:58:40 +0200

    Correction orthographe
    J'ai fais les corrections que je t'avais proposé sur le forum  plus celle du "Message du nombre d'ajout par jour". Pour la ligne 103, je ne suis pas sur s'il faut accorder le verbe "posté" , vu que c'est plus un terme qui vient de l'anglais "post".. Je demanderai au petit barbu ojd, il est pas mauvais en correction.

--- resources/lang/fr/home.php
+++ resources/lang/fr/home.php
@@ -9,7 +9,7 @@ return [
     //****************************
 
     'titleCheckCragInformation' => 'Check les infos des falaises',
-    'descriptionCheckCragInformation' => 'Oblyk est une grande base de donnée des falaises d\'escalades de france et du monde constament enrichie par la communauté, tu peux consulter celle-ci en utilisant le recherche ou en navigant sur la carte des falaises.',
+    'descriptionCheckCragInformation' => 'Oblyk est une grande base de donnée des falaises d\'escalade de France et du monde constamment enrichie par la communauté, tu peux consulter celle-ci en utilisant le recherche ou en naviguant sur la carte des falaises.',
     'actionCheckCragInformation' => 'La carte des falaises',
 
 
@@ -19,8 +19,8 @@ return [
 
     'titleCrossBook' => 'Tiens un carnet de croix',
     'descriptionCrossBook' => '
-        Bon pour la mémoire, la progression et la motivation, tenir un carnet de croix à beaucoup d\'avantage.<br>
-        Oblyk te permet de facilement faire ça, tu pourras ensuite visualiser ta progression grâce à de nombres façon d\'analyser et afficher ton carnet de croix
+        Bon pour la mémoire, la progression et la motivation, tenir un carnet de croix à beaucoup d\'avantages.<br>
+        Oblyk te permet de le faire facilement, tu pourras ensuite visualiser ta progression grâce à de nombreuses façons d\'analyser et d\'afficher ton carnet de croix
     ',
 
 
@@ -30,8 +30,8 @@ return [
 
     'titlePartner' => 'Trouve un partenaire de grimpe',
     'descriptionPartner' => '
-        Tu arrive dans une nouvelle regions, tu aimerais trouver quelqu\'un avec qui grimper en falaise ou en salle ?<br>
-        Regarde du côter de notre carte des grimpeurs, peut-être que tu trouveras quelqu\'un avec qui grimper.
+        Tu arrives dans une nouvelle région, tu aimerais trouver quelqu\'un avec qui grimper en falaise ou en salle ?<br>
+        Regarde du côté de notre carte des grimpeurs, peut-être que tu trouveras quelqu\'un avec qui grimper.
     ',
     'actionPartner' => 'En savoir plus',
 
@@ -42,9 +42,9 @@ return [
 
     'titleNewsFeed' => 'Reste au courant',
     'descriptionNewsFeed' => '
-        Oblyk intégre un système de fil d\'acualité simple qui te permet d\'être au courant de l\'activité des falaises autour de chez toi.<br>
+        Oblyk intègre un système de fil d\'actualité simple qui te permet d\'être au courant de l\'activité des falaises autour de chez toi.<br>
         Par exemple s\'il y a une mission "Nettoyage Printanier", l\'organisateur post dans le flux de la falaise et hop !
-        tous les intéréssés sont au courant et peuvent venir aider.
+        tous les intéressés sont au courant et peuvent venir aider.
     ',
 
 
@@ -54,9 +54,9 @@ return [
 
     'titleAccount' => 'Rejoins-nous !',
     'descriptionAccount' => '
-        En rejoingnant oblyk tu auras librement accès à tous les infos de la communauté, te tenir au courant de ce qui se passe dans le
-        monde de la grimpe, tu pourras tenir un carnet de croix pour t\'aider à progresser ou pour garder un souvenire de tes passages en falaise.<br>
-        Et tu pouras participer à l\'élaboration d\'une grande base de connaissance commune des sites d\'escalades de France et du monde !
+        En rejoignant Oblyk tu auras librement accès à toutes les infos de la communauté, tu pourras te tenir au courant de ce qui se passe dans le
+        monde de la grimpe, tu pourras tenir un carnet de croix pour t\'aider à progresser ou pour garder un souvenir de tes passages en falaise.<br>
+        Et tu pourras participer à l\'élaboration d\'une grande base de connaissance commune des sites d\'escalade de France et du monde !
     ',
     'actionAccount' => 'Créer un compte',
 
@@ -67,10 +67,10 @@ return [
 
     'titleGuidebook' => 'Oblyk et les topos',
     'descriptionGuidebook' => "
-        Vous pourriez vous demander si oblyk peut remplacer les topos papier et par extension nuire au développement des sites d'escalde ?<br>
-        Sachez que nous nous somme beaucoup posé cette question aussi. Et la réponse est non, Oblyk ne peut pas remplacer un topo !<br>
-        Les topos continent des informations indispensable que nous ne permettons pas d'ajouter sur oblyk<br><br>
-        Acheter un topo, c'est soutenir des passionnés qui donnent énormément pour maintenir, développer, nettoyer les sites d'escalade sur lesquel vous allez grimper gratuitement.
+        Vous pourriez vous demander si Oblyk peut remplacer les topos papier et par extension nuire au développement des sites d'escalade ?<br>
+        Sachez que nous nous sommes beaucoup posé cette question aussi. Et la réponse est non, Oblyk ne peut pas remplacer un topo !<br>
+        Les topos contiennent des informations indispensables que nous ne permettons pas d'ajouter sur oblyk<br><br>
+        Acheter un topo, c'est soutenir des passionnés qui donnent énormément pour maintenir, développer, nettoyer les sites d'escalade sur lesquels vous allez grimper gratuitement.
     ",
 
     //Quoi de neuf
@@ -92,12 +92,12 @@ return [
     'figuresVideos' => 'Vidéos',
 
     //Message du nombre d'ajout par jour
-    'crag_today'=>'Une falaise a été ajouté aujourd\'hui|:count falaises ont été ajoutées aujourd\'hui ',
+    'crag_today'=>'Une falaise a été ajoutée aujourd\'hui|:count falaises ont été ajoutées aujourd\'hui ',
     'user_today'=>'Un nouveau grimpeur est arrivé aujourd\'hui|:count grimpeurs sont arrivés aujourd\'hui ',
-    'gym_today'=>'Une nouvelle salle a été ajouté aujourd\'hui|:count salles ont été ajoutées aujourd\'hui ',
-    'route_today'=>'Une nouvelle ligne a été ajouté aujourd\'hui|:count lignes ont été ajoutées aujourd\'hui ',
+    'gym_today'=>'Une nouvelle salle a été ajoutée aujourd\'hui|:count salles ont été ajoutées aujourd\'hui ',
+    'route_today'=>'Une nouvelle ligne a été ajoutée aujourd\'hui|:count lignes ont été ajoutées aujourd\'hui ',
     'cross_today'=>'Une nouvelle croix a été faite aujourd\'hui|:count croix ont été faites aujourd\'hui ',
-    'photo_today'=>'Une nouvelle photo a été ajouté aujourd\'hui|:count photos ont été ajoutées aujourd\'hui ',
+    'photo_today'=>'Une nouvelle photo a été ajoutée aujourd\'hui|:count photos ont été ajoutées aujourd\'hui ',
     'topo_today'=>'Un nouveau topo a été référencé aujourd\'hui|:count topos ont été référencés aujourd\'hui ',
     'description_today'=>'Un nouveau commentaire a été posté aujourd\'hui|:count commentaires ont été postés aujourd\'hui ',
     'video_today'=>'Une nouvelle vidéo a été posté aujourd\'hui|:count vidéos ont été ajoutées aujourd\'hui ',
