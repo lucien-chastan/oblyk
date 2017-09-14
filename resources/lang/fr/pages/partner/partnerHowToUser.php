0a1f867151860607315570dacafeb28e3983641f
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 10:07:14 +0200

    Update partnerHowToUser.php

--- resources/lang/fr/pages/partner/partnerHowToUser.php
+++ resources/lang/fr/pages/partner/partnerHowToUser.php
@@ -5,29 +5,29 @@ return [
     //Intro
     'bigTitle' => "La recherche de partenaire",
     'slogan' => "Comment ça marche ?",
-    'intro' => "Tu arrive dans une nouvelle région, tu cherche quelqu'un pour t'accompagner lors de tes sorties falaises ou pour aller grimper à la salle du coin ? Tu trouvera peut-être ton bonheur ici !",
-    'introNoCompte' => "Pour voir les grimpeurs inscrits sur la recherche de partenaire, il faut déjà que tu es un compte !",
+    'intro' => "Tu arrives dans une nouvelle région, tu cherches quelqu'un pour t'accompagner lors de tes sorties falaises ou pour aller grimper à la salle du coin ? Tu trouveras peut-être ton bonheur ici !",
+    'introNoCompte' => "Pour voir les grimpeurs inscrits sur la recherche de partenaire, il faut déjà que tu aies un compte !",
     'loginAction' => "Se connecter",
     'registerAction' => "Créer un compte",
 
     //Etape 1 :
     'title_step_1' => "Étape 1 : Mes préférences",
-    'description_step_1' => "Pour donner envie aux autres de te contacter il faut qu'ils aient un minimum d'informations sur toi, quel style d'escalade tu pratique (bloc, voie, trad, ...), dans quelle cotation tu aime grimper, etc.  ",
+    'description_step_1' => "Pour donner envie aux autres de te contacter il faut qu'ils aient un minimum d'informations sur toi, quel style d'escalade tu pratiques (bloc, voie, trad, ...), dans quelle cotation tu aimes grimper, etc.  ",
     'action_step_1' => "Qui je suis ?",
 
     //Etape 2 :
     'title_step_2' => "Étape 2 : Ma zone de grimpe",
-    'description_step_2' => "Pour que nous puissions te présenter une liste des grimpeurs près de chez toi, faut-il déjà que nous sachions où tu grimpe !<br>Tu peux établire une liste des lieux où tu grimpe, et oblyk te présentera les grimpeurs qui partage les mêmes zones que toi.",
+    'description_step_2' => "Pour que nous puissions te présenter une liste des grimpeurs près de chez toi, faut-il déjà que nous sachions où tu grimpes !<br>Tu peux établir une liste des lieux où tu grimpes, et Oblyk te présentera les grimpeurs qui partagent les mêmes zones que toi.",
     'action_step_2' => "Mes lieux ?",
 
     //Etape 3 :
     'title_step_3' => "Étape 3 : La carte des grimpeurs",
-    'description_step_3' => "Maintenant que tu as renseigné tes lieux d'escalade et dit qui tu étais, tu peux utiliser la carte des grimpeurs pour trouver quelqu'un avec qui tu aimerais grimper.",
+    'description_step_3' => "Maintenant que tu as renseigné tes lieux d'escalade et dis qui tu étais, tu peux utiliser la carte des grimpeurs pour trouver quelqu'un avec qui tu aimerais grimper.",
     'action_step_3' => "La carte des grimpeurs",
 
     //Etape 4 :
     'title_step_4' => "Étape 4 : Contact",
-    'description_step_4' => "Tu as trouver quelqu'un avec qui tu aimerais grimper ? Envoie lui un message via la messagerie d'oblyk.<br>Libre à vous ensuite de vous organiser comme vous voulez.",
+    'description_step_4' => "Tu as trouvé quelqu'un avec qui tu aimerais grimper ? Envoie lui un message via la messagerie d'Oblyk.<br>Libre à vous ensuite de vous organiser comme vous le voulez.",
     'action_step_4' => "Ma messagerie",
 
 ];
