424752e928690e88a9430c678fc8618dfb787189
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 10:14:03 +0200

    Update category.php

--- resources/lang/fr/pages/forums/category.php
+++ resources/lang/fr/pages/forums/category.php
@@ -6,10 +6,10 @@ return [
     //titre et intro
     'title' => "Les catégories",
     'intro_1' => "Dans cette section, vous trouverez toutes les catégories de discussions qui existent sur le forum d'escalade d'Oblyk: résumés de voyages de grimpe, questions relatives à une voie ou un site d'escalade, etc.",
-    'intro_2' => "Si vous ne trouvez la catégorie parfaite pour votre sujet, utiliser la \"zone d'expression libre\", elle est faite pour ça.",
+    'intro_2' => "Si vous ne trouvez pas la catégorie parfaite pour votre sujet, utilisez la \"zone d'expression libre\", elle est faite pour ça.",
 
     //les boites
     'nbTopics'=>"{0}pas encore de sujet posté|{1}un sujet posté|[2,*]:count sujet posté",
     'btnCreateTopics'=>"Créer un sujet",
-    'btnSeeTopics'=>"voir les sujets",
+    'btnSeeTopics'=>"Voir les sujets",
 ];
