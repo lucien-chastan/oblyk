90cb0a7d6785e7d0dab0113c2df66031efbbdc54
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 10:35:09 +0200

    Update partnerSearch.php

--- resources/lang/fr/pages/profile/partnerSearch.php
+++ resources/lang/fr/pages/profile/partnerSearch.php
@@ -6,15 +6,15 @@ return [
     'titleWhoIAm' => "Qui je suis ?",
     'explication' => "
     <strong class=\"text-underline\">Pourquoi cette page ?</strong><br>
-    Pour que d'autres grimpeurs et envie de te contacter, il est important qu'ils aient un minimum d'informations sur toi.
-    Cette page te permet de donne quelques indicateurs de qui tu es, ton niveau, quelle type de grimpe tu pratique, etc.
+    Pour que d'autres grimpeurs aient envie de te contacter, il est important qu'ils aient un minimum d'informations sur toi.
+    Cette page te permet de donner quelques indicateurs de qui tu es, ton niveau, quelle type de grimpe tu pratiques, etc.
     ",
 
     //Step 1 : Faire partie de la recherche de partenaire
-    'step1Title' => "1. Faire partie de la recherche de partenaire d'oblyk",
+    'step1Title' => "1. Faire partie de la recherche de partenaire d'Oblyk",
     'labelActive' => "Activer ma recherche de partenaire de grimpe",
     'titleNote' => "Note :",
-    'li1' => "Si cette case n'est pas cochée, tu n'apparaîtra pas sur la carte des grimpeurs",
+    'li1' => "Si cette case n'est pas cochée, tu n'apparaîtras pas sur la carte des grimpeurs",
     'li2' => "Désactiver ton profil de la recherche de partenaire ne supprime pas tes préférences ni tes lieux de grimpe, tu deviens juste invisible.",
 
     //Step 2 : Mes types de grimpes
@@ -23,14 +23,14 @@ return [
     //Step 3 : Niveau d'escalade
     'step3Title' => "3. Quel est mon niveau ?",
     'minLabel' => "Mon niveau minimum",
-    'maxLabel' => "Mon niveau maxium",
-    'noteLevel' => "Note : si tu ne sais pas quoi mettre, ton niveau minium pourait correspondre à ton échauffement, et ton niveau max à tes projets.",
+    'maxLabel' => "Mon niveau maximum",
+    'noteLevel' => "Note : si tu ne sais pas quoi mettre, ton niveau minimum pourrait correspondre à ton échauffement, et ton niveau max à tes projets.",
 
 
     //Step 4 : Description
-    'step4Title' => "4. Pourquoi ne pas parler un peut plus de toi ?",
+    'step4Title' => "4. Pourquoi ne pas parler un peu plus de toi ?",
     'LabelWhoIAm' => "Qui je suis avec mes mots",
-    'noteDescription' => "Note : tu peux par exemple préciser ce que tu recherche, te décrir plus amplement, parler de ton matériel d'escalade ou de tes connaissances dans les manipes par exemple.",
+    'noteDescription' => "Note : tu peux par exemple préciser ce que tu recherches, te décrire plus amplement, parler de ton matériel d'escalade ou de tes connaissances dans les manips par exemple.",
 
     //final
     'submit' => "Enregistrer",
