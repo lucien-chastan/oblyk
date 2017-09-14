51c7b586bdd2ff32bcd38bc1ac5b0d32b90e1a01
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 10:26:42 +0200

    Update post.php

--- resources/lang/fr/pages/post/post.php
+++ resources/lang/fr/pages/post/post.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'noActuality' => "Il n'y a pas encore d'acutalité postée ici",
+    'noActuality' => "Il n'y a pas encore d'actualité postée ici",
     'postEnd' => "fin des posts",
     'addActuality' => "Poster dans le fil d'actualité",
     'postActuality' => "Poster une actualité",
