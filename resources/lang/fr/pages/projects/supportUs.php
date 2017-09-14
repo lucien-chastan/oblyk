d1937b52b8615dbeb4c9af92ddcd649daa7f495a
Cdriss <31932438+Cdriss@users.noreply.github.com>
2017-09-14 10:43:58 +0200

    Update supportUs.php

--- resources/lang/fr/pages/projects/supportUs.php
+++ resources/lang/fr/pages/projects/supportUs.php
@@ -3,15 +3,15 @@
 return [
 
     'title' => 'Nous soutenir',
-    'para_1' => "Sur Oblyk il n'y a aucune limitation, pas de compte prenium, ni pub, ni lien ou article sponsorisé, tous est gratuit ! ; )",
-    'para_2' => "Si tout est gratuit et qu'obyk ne perçoit aucun revenu, cela veut-il dire qu'oblyk ne coûte rien à développer ?",
+    'para_1' => "Sur Oblyk il n'y a aucune limitation, pas de compte premium, ni pub, ni lien ou article sponsorisé, tous est gratuit ! ; )",
+    'para_2' => "Si tout est gratuit et qu'Oblyk ne perçoit aucun revenu, cela veut-il dire qu'Oblyk ne coûte rien à développer ?",
     'para_3' => "
-    Malheureusement non, comme tout site internet, oblyk doit être hebergé dans un datacenter et louer un nom de domaine pour pouvoir éxister sur la toile.<br>
-    Et ça par contre se n'est pas gratuit ...
+    Malheureusement non, comme tout site internet, Oblyk doit être hébergé dans un datacenter et louer un nom de domaine pour pouvoir éxister sur la toile.<br>
+    Et ça par contre, ce n'est pas gratuit ...
     ",
     'para_4' => "
-    Si vous avez quelques euros en trop, que vous appréciez le travail réalisé et que vous souhaitez encourager le développement d'oblyk, vous pouvez nous faire un don ; )<br>
-    C'est pour nous une aide inestimable ! Vous nous permetrez ainsi de continuez le développement de cette belle platforme et d'assurer son avenir ; )
+    Si vous avez quelques euros en trop, que vous appréciez le travail réalisé et que vous souhaitez encourager le développement d'Oblyk, vous pouvez nous faire un don ; )<br>
+    C'est pour nous une aide inestimable ! Vous nous permettrez ainsi de continuer le développement de cette belle plateforme et d'assurer son avenir ; )
     ",
     'thanks'=>"Merci d'avance de votre générosité !",
 
