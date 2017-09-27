1895a778c8f4b5340f0b1c2a06c19fe24ece30f0
lucien-chastan <chastanlucien@gmail.com>
2017-09-27 09:00:14 +0200

    resolution problème marche approach

--- resources/lang/fr/modals/approach.php
+++ resources/lang/fr/modals/approach.php
@@ -2,11 +2,11 @@
 
 return [
 
-    'editTooltip' => 'Modifier cette marche d\'approche',
-    'addTooltip' => 'Ajouter une marche d\'approche',
-    'deleteTooltip' => 'Supprimer cette marche d\'approche',
-    'modalEditeTitle' => 'Modifier la marche d\'approche',
-    'modalAddTitle' => 'Ajouter une marche d\'approche',
+    'editTooltip' => "Modifier cette marche d\'approche",
+    'addTooltip' => "Ajouter une marche d\'approche",
+    'deleteTooltip' => "Supprimer cette marche d\'approche",
+    'modalEditeTitle' => "Modifier la marche d\'approche",
+    'modalAddTitle' => "Ajouter une marche d\'approche",
 
     'postByDate' => 'ajouté par <a href=":url">:name</a> le :date',
 
