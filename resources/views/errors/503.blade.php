67d7b9757b93647ac8afee04aa2b520dd2ee301c
lucien-chastan <chastanlucien@gmail.com>
2017-10-08 15:15:56 +0200

    page 503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title>Oblyk est en maintenance</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="description" content="Nous sommes entrain de faire des traveaux sur Oblyk, revient dans quelques temps pour retrouvé un Oblyk meilleurs que jamais">
+    <meta name="author" content="Lucien CHASTAN">
+    <link rel="icon" href="/img/fav_icon.png">
+
+    <meta property="og:title" content="Oblyk est en maintenance"/>
+    <meta property="og:description" content="Nous sommes entrain de faire des traveaux sur Oblyk, revient dans quelques temps pour retrouvé un Oblyk meilleurs que jamais"/>
+    <meta property="og:image" content="https://oblyk.org/img/oblyk-home-baume-rousse.jpg" />
+    <meta property="og:type" content="website"/>
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    {{--css globale de l'application--}}
+    <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/materialize.css" rel="stylesheet">
+
+
+    <style>
+
+        h1 {margin-top: 10vh;}
+        img {width: 500px;}
+
+    </style>
+</head>
+<body>
+
+    <main>
+
+        {{--contenu de la page--}}
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h1 class="loved-king-font text-center grey-text text-darken-3">Oblyk est en maintenance !</h1>
+
+                    <div class="row text-center">
+                        <img alt="des grimpeurs qui brossent un bloc" class="responsive-img" src="/img/maintenance.svg">
+                    </div>
+
+                    <p class="grey-text text-center">
+                        Nous sommes en train de faire des travaux sur Oblyk, revient dans quelque temps pour retrouver un Oblyk meilleurs que jamais !!
+                    </p>
+
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+</html>
