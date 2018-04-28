990b3dc94f8939c4e131fd0eb20b950a9f4e4f39
lucien-chastan <chastanlucien@gmail.com>
2018-04-28 13:21:28 +0200

    ajout d'un lien dans le galerie photo du bashboard

--- resources/views/pages/profile/vues/dashboardBox/boxVues/photos-last.blade.php
+++ resources/views/pages/profile/vues/dashboardBox/boxVues/photos-last.blade.php
@@ -1,5 +1,5 @@
 <div id="lastPhototheque" class="phototheque">
     @foreach($photos as $photo)
-        <img data-full="/storage/photos/crags/1300/{{$photo->slug_label}}" data-legende="{{$photo->description}}" src="/storage/photos/crags/100/{{$photo->slug_label}}" alt="{{$photo->description}}">
+        <img data-full="/storage/photos/crags/1300/{{$photo->slug_label}}" data-legende="{{$photo->description}}<br><a href='{{ $photo->getTargetLink()['link'] }}'>{{ $photo->getTargetLink()['name'] }}</a>"" src="/storage/photos/crags/100/{{$photo->slug_label}}" alt="{{$photo->description}}">
     @endforeach
 </div>
