226195251ca1150b65f69ac85fd800509c46d1cb
lucien-chastan <chastanlucien@gmail.com>
2018-09-23 19:50:45 +0200

    Write the developer page

--- resources/lang/fr/pages/projects/developer.php
+++ resources/lang/fr/pages/projects/developer.php
@@ -2,6 +2,16 @@
 
 return [
 
-    'title' => 'Développeur &amp; API',
+    'title' => "Développeur &amp; API",
 
+    'titleContribute' => "Contribuer au &#139;/code&#155;",
+    'paragraphContribute' => "Oblyk est un site open-source, si vous voulez contribuer à l'amélioration d'oblyk ? Rendez-vous sur la page <a href=\"https://github.com/lucien-chastan/oblyk\">Github</a> du projet pour en savoir plus.",
+
+    'titleAPI' => "Utiliser l'API",
+    'paragraphAPI' => "Petit à petit nous rendons accessible les données d'oblyk via notre API pour que vous puissiez les utiliser dans d'autres applications. Si vous avez un besoin en particulier n'hésitez pas à nous la soumettre, si votre projet nous plaît, nous serons heureux de vous développer une route ! <br>Pour utiliser notre API, rendez vous sur la documentation : <a target=\"_blank\" href=\"https://api.oblyk.org\">api.oblyk</a>",
+
+    'titleIframe' => "Les &#139;/iframes&#155;",
+    'paragraphIframe' => "Autre moyen de profiter de la base de donnée pour votre site est d'utiliser les iframes. Rendez-vous sur la page d'une falaise, trouver le bouton <cite>\"Partager\"</cite> et récupérer le code de l'iframe à intégrer à votre site.<br> Vous aurez un rendu équivalent à celui-ci :",
+
+    'codeExample' => "Exemple de code :",
 ];
--- resources/views/pages/project/developer.blade.php
+++ resources/views/pages/project/developer.blade.php
@@ -15,8 +15,27 @@
             <div class="col s12">
                 <h1 class="loved-king-font text-center grey-text text-darken-3">@lang('pages/projects/developer.title')</h1>
 
-                <p class="text-center grey-text">Coming soon</p>
+                <h3 class="loved-king-font">@lang('pages/projects/developer.titleContribute')</h3>
 
+                <p>
+                    @lang('pages/projects/developer.paragraphContribute')
+                </p>
+
+                <h3 class="loved-king-font">@lang('pages/projects/developer.titleAPI')</h3>
+
+                <p>
+                    @lang('pages/projects/developer.paragraphAPI')
+                </p>
+
+                <h3 class="loved-king-font">@lang('pages/projects/developer.titleIframe')</h3>
+                <p>
+                    @lang('pages/projects/developer.paragraphIframe')
+                </p>
+
+                <iframe src="https://oblyk.org/iframe/crag/18" width="100%" height="150px" frameborder="0"></iframe>
+
+                <p class="text-underline">@lang('pages/projects/developer.codeExample')</p>
+                <pre>{{ '<iframe src="https://oblyk.org/iframe/crag/{id}" width="100%" height="150px" frameborder="0"></iframe>' }}</pre>
             </div>
         </div>
     </div>
