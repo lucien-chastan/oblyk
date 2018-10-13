076ae1c4c46525a17ee4af424a197115a636d641
lucien-chastan <chastanlucien@gmail.com>
2018-10-13 10:30:12 +0200

    nouvel article : les gorges du tarn

---
+++ resources/views/pages/article/article/gorge-du-tarn.blade.php
@@ -0,0 +1,260 @@
+{{-- CARACTÉRISTIQUE --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">notes</i>
+    Caractéristiques
+</h2>
+
+<p>
+    Les gorges du Tarn sont un site naturel splendide de par leur nature.
+</p>
+<p>
+    Ce site offre de magnifiques falaises composées de nombreux trous en dolomie calcaire avec un grand nombre
+    de profils différents.
+</p>
+<p>
+    Cela va d'immenses parois verticales comme le secteur De que fas aqui ?
+    à des profils très déversants comme Oasif ou encore Tennessee.
+</p>
+
+<p>
+    Sur ce site, il y a plus de 700 voies principalement dans les cotations allant du 6 au 8.
+    Il y a vraiment un très grand nombre de voies dans le 7ème degré avec une très grande variété.
+</p>
+
+{{-- SAISONS --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">date_range</i>
+    Saisons favorables
+</h2>
+
+
+<p>
+    La mi-saison est la meilleure période pour ce site. Il est donc bien souvent très prisé à cette période.
+</p>
+
+<p>
+    Il est tout de même possible de grimper toute l'année en jouant sur les différentes expositions
+    qu'offrent ces gorges.
+</p>
+
+{{-- LOCALISATION ET TOPO --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">place</i>
+    Localisation &amp; Topo
+</h2>
+
+<p>
+    Les gorges du Tarn se situe sur la commune de Saint Georges de Lévéjac en Lozère, à la frontière avec l'Aveyron.
+</p>
+
+<p>
+    Le dernier topo de la falaise est paru en 2012, mais de nombreuses nouvelles voies sont présentes avec
+    pour certaines les cotations indiquées au pied, mais pas toujours.
+    <a href="https://oblyk.org/fr/topo-escalade/205/les-gorges-du-tarn">Topo des gorges du Tarn</a>
+    Des informations complémentaires sont disponibles ici :
+    <a href="http://www.topo-tarn-jonte-dourbie.info">www.topo-tarn-jonte-dourbie.info</a>
+</p>
+
+@include('pages.article.article.partial.topos-crags')
+
+
+{{-- PAS DE SOUCY - GORGES DU TARN --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">terrain</i>
+    Gorges du Tarn : Pas de Soucy (Noir Désir)
+    <a class="voir-sur-oblyk" href="{{ \App\Crag::webUrl(1873,'pas-de-soucy') }}">voir sur oblyk</a>
+</h2>
+
+<p>
+    Sur ce secteur dominé par le <strong>Roc Aiguille</strong>, où il y a quelques grandes voies,
+    j'ai pu découvrir le secteur <strong>Noir Désir</strong>.
+    Il est exposé plein est, ce qui permet d'avoir le soleil le matin sur la falaise.
+</p>
+
+<p>
+    Ce secteur comporte des voies entre 20 et 30 m allant du <strong class="color-grade-31">6a</strong> au <strong class="color-grade-39">7b</strong>.
+</p>
+
+<div class="row">
+    <div class="col s12">
+        <img alt="Gorge du Tarn – Roc Aiguille" title="Gorge du Tarn – Roc Aiguille" src="/storage/articles/photos/tarn/tarn-roc-aiguille.jpg" class="responsive-img">
+        <p class="text-center grey-text">Roc Aiguille</p>
+    </div>
+</div>
+
+@include('pages.article.article.partial.article-graph', ['crag_id' => '1873'])
+
+{{-- BAUMES CHAUDES - GORGES DU TARN --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">terrain</i>
+    Gorges du Tarn : Baumes Chaudes (Foetus)
+    <a class="voir-sur-oblyk" href="{{ \App\Crag::webUrl(1874,'baumes-chaudes') }}">voir sur oblyk</a>
+</h2>
+
+<p>
+    Ce secteur nommé <strong>Fœtus</strong> comporte des voies assez courtes et assez atypiques.
+    Le soleil y est bien présent dès la fin de matinée.
+</p>
+
+<div class="row">
+    <div class="col s12">
+        <img alt="Gorge du Tarn – Baumes Chaudes" title="Gorge du Tarn – Baumes Chaudes" src="/storage/articles/photos/tarn/tarn-baumes-chaudes.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue sur le secteur Fœtus</p>
+    </div>
+</div>
+
+@include('pages.article.article.partial.article-graph', ['crag_id' => '1874'])
+
+
+{{-- BAUMES BASSES - GORGES DU TARN --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">terrain</i>
+    Gorges du Tarn : Baumes Basses (De que fas aqui ?)
+    <a class="voir-sur-oblyk" href="{{ \App\Crag::webUrl(1875,'baumes-basses') }}">voir sur oblyk</a>
+</h2>
+
+<p>
+    <strong>De que fas aqui ?</strong> est une immense paroi est impossible à louper !! Elle est majestueuse et en impose. J'ai été impressionnée !<br>
+    Elle est bien prisée car elle comporte de nombreuses voies allant du <strong class="color-grade-29">5c</strong> au projet sur des longueurs de voies jusqu'à 100m.
+</p>
+
+<div class="row">
+    <div class="col s12">
+        <img alt="Gorge du Tarn – Baumes Basses" title="Gorge du Tarn – Baumes Basses" src="/storage/articles/photos/tarn/tarn-baumes-basses.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue sur le secteur De que fas aqui ?</p>
+    </div>
+</div>
+
+@include('pages.article.article.partial.article-graph', ['crag_id' => '1875'])
+
+
+
+{{-- CIRQUE DES BAUMES - GORGES DU TARN --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">terrain</i>
+    Gorges du Tarn : Cirque des baumes (Amphithéâtre, Tennessee, Navire)
+    <a class="voir-sur-oblyk" href="{{ \App\Crag::webUrl(1876,'cirque-des-baumes') }}">voir sur oblyk</a>
+</h2>
+
+<p>
+    <strong>Tennessee</strong> est un des secteurs les plus reconnus surtout avec la voie éponyme,
+    <strong class="color-grade-45">8b</strong> et le fameux <strong class="color-grade-43">8a</strong>
+    <a href="{{ \App\Route::webUrl(65550,'les-ailes-du-desir') }}">"Les Ailes du désirs"</a>.<br>
+    Au niveau de ce secteur se dresse une petite chapelle de Saint Hilaire.
+</p>
+
+<div class="row">
+    <div class="col s12 m6">
+        <img alt="Secteur Tennessee et sa petite chapelle" title="Secteur Tennessee et sa petite chapelle" src="/storage/articles/photos/tarn/tarn-chapelle-tennessee.jpg" class="responsive-img">
+        <p class="text-center grey-text">Secteur Tennessee et sa petite chapelle</p>
+    </div>
+    <div class="col s12 m6">
+        <img alt="Vue au-dessus de Tennessee" title="Vue au-dessus de Tennessee" src="/storage/articles/photos/tarn/tarn-tennesse.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue au-dessus de Tennessee</p>
+    </div>
+</div>
+
+<p>
+    <strong>Navire</strong> est un autre secteur du cirque des Baumes. Il comporte de belles envolées sur un beau surplomb déversant avec de nombreux bacs. L'escalade y est athlétique et dynamique.<br>
+    Le secteur est assez haut et propose une belle vue encore une fois.<br>
+    Il est à l'ombre assez rapidement dans l'après-midi.
+</p>
+
+<div class="row">
+    <div class="col s12 m6 offset-m3">
+        <img alt="Vue d'une partie du secteur Navire" title="Vue d'une partie du secteur Navire" src="/storage/articles/photos/tarn/tarn-navire.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue d'une partie du secteur Navire</p>
+    </div>
+</div>
+
+<p>
+    <strong>Amphithéâtre</strong> est un secteur à gauche de Tennessee qui résurge souvent et qui comporte un petit air même en pleine chaleur.
+    Le pied des voies n'est pas toujours très facile mais les voies sont jolies. La vue depuis le secteur est vraiment magnifique.
+</p>
+
+<div class="row">
+    <div class="col s12 m9">
+        <img alt="Secteur Amphithéâtre – le Tarn" title="Secteur Amphithéâtre – le Tarn" src="/storage/articles/photos/tarn/tarn-amphitheatre-riviere.jpg" class="responsive-img">
+        <p class="text-center grey-text">Secteur Amphithéâtre – le Tarn</p>
+    </div>
+    <div class="col s12 m3">
+        <img alt="Amphithéâtre – partie droite" title="Amphithéâtre – partie droite" src="/storage/articles/photos/tarn/tarn-amphitheatre-visages.jpg" class="responsive-img">
+        <p class="text-center grey-text">Amphithéâtre – partie droite</p>
+    </div>
+    <div class="col s12">
+        <img alt="Vue sur le secteur Amphithéâtre – partie de gauche" title="Vue sur le secteur Amphithéâtre – partie de gauche" src="/storage/articles/photos/tarn/tarn-amphitheatre.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue sur le secteur Amphithéâtre – partie de gauche</p>
+    </div>
+</div>
+
+@include('pages.article.article.partial.article-graph', ['crag_id' => '1876'])
+
+
+{{-- BAUMES HAUTES - GORGES DU TARN --}}
+
+<h2 class="loved-king-font">
+    <i class="material-icons left deep-orange-text text-darken-2">terrain</i>
+    Gorges du Tarn : Baumes hautes (Oasif, Planète causse)
+    <a class="voir-sur-oblyk" href="{{ \App\Crag::webUrl(165,'baumes-hautes') }}">voir sur oblyk</a>
+</h2>
+
+<p>
+    <strong>Oasif</strong> et <strong>Planète Causse</strong> sont deux secteurs emblématiques.<br>
+    Le premier est vraiment remarquable de par son dévers et au pied, il y avait une sorte de mini étendue d'eau lors de mon passage.
+</p>
+
+<div class="row">
+    <div class="col s12">
+        <img alt="Vue sur le secteur Amphithéâtre – partie de gauche" title="Vue sur le secteur Amphithéâtre – partie de gauche" src="/storage/articles/photos/tarn/tarn-oasif.jpg" class="responsive-img">
+        <p class="text-center grey-text">Vue du secteur Oasif</p>
+    </div>
+</div>
+
+<p>
+    <strong>Planète Causse</strong> est un grand mur orangé bien travaillé proposant une escalade physique.
+    La grande majorité des voies sont dans le 7 plutôt <strong class="color-grade-39">7b</strong> et <strong class="color-grade-41">7c</strong> sur 30-35m, en légers dévers.<br>
+    Ce secteur est exposé au soleil dès la fin de la matinée.
+</p>
+
+<div class="row">
+    <div class="col s12 m3">
+        <img alt="Amphithéâtre – partie droite" title="Amphithéâtre – partie droite" src="/storage/articles/photos/tarn/tarn-planete-causse-petit-dejeuner.jpg" class="responsive-img">
+        <p class="text-center grey-text">Amphithéâtre – partie droite</p>
+    </div>
+    <div class="col s12 m9">
+        <img alt="Secteur Amphithéâtre – le Tarn" title="Secteur Amphithéâtre – le Tarn" src="/storage/articles/photos/tarn/tarn-planete-causse.jpg" class="responsive-img">
+        <p class="text-center grey-text">Secteur Amphithéâtre – le Tarn</p>
+    </div>
+</div>
+
+@include('pages.article.article.partial.article-graph', ['crag_id' => '165'])
+
+<p>
+    L'escalade dans les gorges du Tarn se révèle très variée tant en profil, qu'en niveau (recommandé dans le 6 tout de même), le tout dans un cadre très agréable.
+</p>
+
+<p>
+    D'autres sites sont assez proches comme le <a href="{{ \App\Crag::webUrl(902, 'le-bouffi') }}">Boffi</a> ou encore <a href="{{ \App\Crag::webUrl(138, 'cantobre') }}">Cantobre</a>. (Sites présents dans le topo <a href="{{ \App\Topo::webUrl(162, 'la-bourdie') }}">La Bourdie</a>)
+</p>
+
+<div class="row">
+    <div class="col s12 m8 offset-m2">
+        <img alt="Boffi" title="Boffi" src="/storage/articles/photos/tarn/tarn-boffi.jpg" class="responsive-img">
+        <p class="text-center grey-text"><a href="{{ \App\Crag::webUrl(902, 'le-bouffi') }}">Boffi</a></p>
+    </div>
+    <div class="col s12 m8 offset-m2">
+        <img alt="Cantobre" title="Cantobre" src="/storage/articles/photos/tarn/tarn-cantobre.jpg" class="responsive-img">
+        <p class="text-center grey-text"><a href="{{ \App\Crag::webUrl(138, 'cantobre') }}">Cantobre</a></p>
+    </div>
+</div>
+
+<p>
+    Vous pouvez retrouver l'article original sur le <a href="https://blog.le-yeti.net/escalade-dans-les-gorges-du-tarn/">blog du Yéti</a>.
+</p>
